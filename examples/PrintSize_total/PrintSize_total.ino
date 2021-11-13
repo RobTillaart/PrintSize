b20ed4c59e87c726abcf45a94885e24a56c4ffde
rob tillaart <rob.tillaart@gmail.com>
2021-11-13 21:00:37 +0100

    update build-CI, readme.md badges, print interface

--- examples/PrintSize_total/PrintSize_total.ino
+++ examples/PrintSize_total/PrintSize_total.ino
@@ -1,12 +1,9 @@
 //
 //    FILE: PrintSize_total.ino
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.1.0
+// VERSION: 0.1.1
 // PURPOSE: demo printSize total counter
 //     URL: https://github.com/RobTillaart/PrintSize
-//
-// HISTORY:
-// 0.1.0   2020-04-30 initial version
 //
 
 /*
@@ -22,12 +19,15 @@
   nisl, porttitor malesuada erat urna eu neque. Phasellus ultricies ante tortor,
   ac facilisis diam dignissim sit amet. Donec accumsan ac orci a malesuada.
 */
+
+
 #include "PrintSize.h"
 
 char woord[24];
-int idx = 0;
+int index = 0;
 
 PrintSize ps;
+
 
 void setup()
 {
@@ -40,6 +40,7 @@ void setup()
   ps.reset();
 }
 
+
 void loop()
 {
   if (Serial.available() > 0)
@@ -49,18 +50,19 @@ void loop()
     {
       process(woord, 60);
       woord[0] = '\0';
-      idx = 0;
+      index = 0;
     }
     else
     {
-      woord[idx++] = c;
-      woord[idx] = '\0';
+      woord[index++] = c;
+      woord[index] = '\0';
     }
   }
 }
 
-// split the stream of words in lines of maxlen
-void process(char * w, uint8_t maxlen)
+
+// split the stream of words in lines of maxLength
+void process(char * w, uint8_t maxLength)
 {
   // skip empty words.
   if (strlen(w) == 0) return;
@@ -71,10 +73,10 @@ void process(char * w, uint8_t maxlen)
   // does the word fit on the line
   ps.print(w);
   ps.print(' ');
-  if (ps.total() >= maxlen)
+  if (ps.total() >= maxLength)
   {
     // if not, fill line with -
-    for (; prev < maxlen; prev++) Serial.print('-');
+    for (; prev < maxLength; prev++) Serial.print('-');
     Serial.println();
 
     // start counter for new line.
@@ -86,4 +88,6 @@ void process(char * w, uint8_t maxlen)
   Serial.print(' ');
 }
 
+
 // -- END OF FILE --
+
